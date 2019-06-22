b1696eb5108d32d788668735e6184faa5124e8c2
Logan Crocker <lcroc001@ucr.edu>
2019-06-21 18:08:01 -0800

    Updates

---
+++ precache-manifest.f4d686fbfb65869237d4db5831ec4c27.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7b07faa6ec3c748d3aa57ef5354bb6f4",
+    "url": "/pokedex/index.html"
+  },
+  {
+    "revision": "5107fec80042eded8134",
+    "url": "/pokedex/static/css/main.51498bbe.chunk.css"
+  },
+  {
+    "revision": "d0d6d07763a2b1156701",
+    "url": "/pokedex/static/js/2.7504aa65.chunk.js"
+  },
+  {
+    "revision": "5107fec80042eded8134",
+    "url": "/pokedex/static/js/main.f5336ed3.chunk.js"
+  },
+  {
+    "revision": "499fa032a833d26aac94",
+    "url": "/pokedex/static/js/runtime~main.fa119ff4.js"
+  }
+]);
